abb75c156c2bfd6e33e0d97a3b708c3e639084c8
polarbearstoty00 <polarbearstoty00@gmail.com>
2025-03-21 17:43:34 +0900

    Update dashboard.py
    v0.3.2

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -32,10 +32,8 @@ if st.session_state.uploaded_files:
     grouped = final_df.groupby('업체명')
     
     for company, company_group in grouped:
-        if st.checkbox(company, key=f'company_{company}'):
+        with st.expander(company):
             product_grouped = company_group.groupby('상품명')
-            product_list = list(product_grouped.groups.keys())
-            selected_product = st.selectbox(f"{company}의 상품 선택", product_list, key=f'product_{company}')
-            
-            if selected_product:
-                st.write(product_grouped.get_group(selected_product))
+            for product in product_grouped.groups.keys():
+                if st.checkbox(product, key=f'product_{company}_{product}'):
+                    st.write(product_grouped.get_group(product))
